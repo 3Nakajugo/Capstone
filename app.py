a189fff5b868bfcb7753a5e08df89d0a024a21ac
3Nakajugo <edinahmargarita@gmail.com>
2020-01-21 12:17:15 +0300

    add models

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
 import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
-# from flask_cors import CORS
+from flask_migrate import Migrate
+from models import db, Movie, Actor
 
-def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__)
-  # CORS(app)
+app = Flask(__name__)
+app.config.from_object('config')
+migrate = Migrate(app, db)
+db.init_app(app)
 
-  return app
-
-APP = create_app()
 
 if __name__ == '__main__':
     APP.run(host='0.0.0.0', port=8080, debug=True)
